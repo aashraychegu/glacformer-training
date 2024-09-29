9b637eb2d980c21938f9916b83fed257795e2633
aashraychegu <aashraychegu@hotmail.com>
2024-09-29 13:34:41 -0700

    Editing logging steps

--- sherlock_run.sh
+++ sherlock_run.sh
@@ -1,3 +1,9 @@
+clear
+echo "Started"
 pip install -r requirements.txt
+clear
+echo "Installed all required packages"
 wandb login 8a2ce1929f5f1d40c72093dc901f3cfe3e945b2e
-python3 training/glacformer_training_script.py
+clear
+echo "Started Training"
+python3 training/glacformer_training_script.py --load_from new --num_epochs 15

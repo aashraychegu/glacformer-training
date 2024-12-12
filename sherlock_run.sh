d1a79966371f7e8e6b4eee33e06a8b49face05e2
aashraychegu <aashraychegu@hotmail.com>
2024-12-11 22:05:20 -0800

    editing run file

--- sherlock_run.sh
+++ sherlock_run.sh
@@ -7,4 +7,4 @@ wandb login 8a2ce1929f5f1d40c72093dc901f3cfe3e945b2e
 clear
 nvidia-smi
 echo "Started Training"
-python3 /home/groups/dustinms/aashrayc/glacformer-training/training/glacformer_training_script.py --load_from new --num_epochs $1
+python3 /home/groups/dustinms/aashrayc/glacformer-training/training/glacformer_training_script.py --continue-training $1 --num_epochs $2

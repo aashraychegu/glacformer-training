b3f74a63d8ef16d494e3441fefdfdc53342a285b
aashraychegu <aashraychegu@hotmail.com>
2024-12-19 14:01:04 -0800

    finishing checkpointing and adding redundancy for environment setup

--- sherlock_run.sh
+++ sherlock_run.sh
@@ -1,11 +1,73 @@
-clear
+#!/bin/bash
+
+# Kill all existing tmux sessions
 echo "Started"
+tmux kill-server
+ps aux | grep python
+clear
+module reset
+module restore simsimd_install
+pip install simsimd
+source $HOME/.bash_profile
 pip install -r requirements.txt
-clear
+pip cache purge
 echo "Installed all required packages"
-wandb login 8a2ce1929f5f1d40c72093dc901f3cfe3e945b2e
-clear
 nvidia-smi
 echo "Started Training"
 git config --global credential.helper store
-python3 /home/groups/dustinms/aashrayc/glacformer-training/training/glacformer_training_script.py --continue_training $1 --num_epochs $2
+export NCCL_DEBUG=INFO
+export NCCL_P2P_LEVEL=NVL
+export script="/home/groups/dustinms/aashrayc/glacformer-training/training/glacformer_training_script.py"
+
+# Default values
+CONTINUE_TRAINING="False"
+NUM_EPOCHS=1
+MASTER_ADDR="127.0.0.1"
+MASTER_PORT=1414
+NUM_NODES=1
+NODE_RANK=0
+WANDB_ACTIVE=0
+SESSION_NAME="GLACFORMER_TRAINING_SESSION_AASHRAYC"
+
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --continue-training) CONTINUE_TRAINING="$2"; shift ;;
+        --epochs) NUM_EPOCHS="$2"; shift ;;
+        --master-addr) MASTER_ADDR="$2"; shift ;;
+        --master-port) MASTER_PORT="$2"; shift ;;
+        --num-nodes) NUM_NODES="$2"; shift ;;
+        --node-rank) NODE_RANK="$2"; shift ;;
+        --wandb-active) WANDB_ACTIVE="$2"; shift ;;
+        --cleanup) TESTING="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [ "$WANDB_ACTIVE" -eq 1 ]; then
+    wandb login 8a2ce1929f5f1d40c72093dc901f3cfe3e945b2e
+else
+    wandb disabled
+fi
+
+# Start tmux session
+#tmux new-session -d -s $SESSION_NAME
+
+# Split the window into two panes vertically
+#tmux split-window -h
+
+# Run nvtop in the left pane
+#tmux send-keys -t $SESSION_NAME:0.0 "nvtop" C-m
+
+# Run the training script in the right pane
+#tmux send-keys -t $SESSION_NAME:0.1 "torchrun --nnodes=$NUM_NODES --nproc-per-node=gpu --master-addr=$MASTER_ADDR --master-port=$MASTER_PORT --node_rank=$NODE_RANK $script --continue_training $CONTINUE_TRAINING --num_epochs=$NUM_EPOCHS" C-m
+
+# Attach to the tmux session
+#tmux attach-session -t $SESSION_NAME
+
+torchrun --nnodes=$NUM_NODES --nproc-per-node=gpu --master-addr=$MASTER_ADDR --master-port=$MASTER_PORT --node_rank=$NODE_RANK $script --continue_training $CONTINUE_TRAINING --num_epochs=$NUM_EPOCHS
+
+if [ "$TESTING" -eq "True" ]; then
+    rm -rf ./glacformer
+fi
--- training/glacformer_training_script.py
+++ training/glacformer_training_script.py
@@ -1,235 +1,243 @@
 # Create the argument parser
-import argparse
-import pathlib
-import torch
-import matplotlib.pyplot as plt
-import numpy as np
-import torch.nn as nn
-import evaluate
-import huggingface_hub
-import albumentations as A
-from datasets import load_dataset
-from transformers import TrainingArguments, Trainer
-from transformers import (
-    SegformerImageProcessor,
-    SegformerConfig,
-    SegformerForSemanticSegmentation,
-)
-import pathlib as pl
-import datetime
-import os
-import accelerate
-import shutil
-
-os.environ["WANDB_PROJECT"] = "glacformer_training"
-os.environ["NCCL_CUMEM_ENABLE"] = "0"
-os.environ["NCCL_DEBUG"] = "INFO"
-torch.backends.cuda.matmul.allow_tf32 = True
-parent_dir = pathlib.Path(__file__).resolve().parent
-
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument(
-    "--continue_training",
-    type=bool,
-    help="continues training from the last checkpoint",
-    default=False,
-)
-
-parser.add_argument(
-    "--load_from",
-    type=str,
-    help="load a new model or from a checkpoint or a path. if 'new' or a path, set continue_training to false",
-    default="new",
-)
-
-parser.add_argument(
-    "--token",
-    type=str,
-    help="The throwaway auth token",
-    default="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",
-)
-parser.add_argument(
-    "--learning_rate",
-    type=float,
-    help="The initial learning rate for Adam",
-    default=18e-5,
-)
-parser.add_argument(
-    "--num_epochs",
-    type=int,
-    help="Total number of training epochs to perform",
-    default=1,
-)
-
-args = parser.parse_args()
-token = args.token
-learning_rate = args.learning_rate
-num_epochs = args.num_epochs
-load_from = args.load_from
-
-print(args.continue_training)
-if args.continue_training == "True":
-    load_from = "checkpoint"
-    print("Continuing training from checkpoint")
-else:
-    print("Starting new training")
-    
-hf_model_name = "glacierscopessegmentation/glacier_segmentation_transformer"
-huggingface_hub.login(token=token, add_to_git_credential=True)
-
-ds = load_dataset("glacierscopessegmentation/scopes", keep_in_memory=True, cache_dir=pl.Path(__file__).parent / "data")
-train_ds = ds["train"]
-test_ds = ds["test"]
-ds.cleanup_cache_files()
-del ds
-
-id2label = {
-    "0": "sky",  # This is given by the rgb value of 00 00 00 for the mask
-    "1": "surface-to-bed",  # This is given by the rgb value of 01 01 01 for the mask
-    "2": "bed-to-bottom",  # This is given by the rgb value of 02 02 02 for the mask
-}
-id2label = {int(k): v for k, v in id2label.items()}
-label2id = {v: k for k, v in id2label.items()}
-num_labels = len(id2label)
-print(len(train_ds), len(test_ds))
-train_ds.cleanup_cache_files()
-test_ds.cleanup_cache_files()
-test_image_processor = SegformerImageProcessor.from_pretrained("nvidia/MiT-b0")
-
-if load_from == "new" or load_from == "checkpoint":
-    test_config = SegformerConfig(
-        num_channels=3,
-        num_labels=num_labels,
-        label2id=label2id,
-        id2label=id2label,
-        depths=[3, 4, 6, 3],
-        hidden_sizes=[64, 128, 320, 512],
-        num_attention_heads=[1, 2, 5, 8],
-        decoder_hidden_size=128 * 6,
+from torch.distributed.elastic.multiprocessing.errors import record
+@record
+def main():
+    import argparse
+    import pathlib
+    import torch
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import torch.nn as nn
+    import evaluate
+    import huggingface_hub
+    import albumentations as A
+    from datasets import load_dataset
+    from transformers import TrainingArguments, Trainer
+    from transformers import (
+        SegformerImageProcessor,
+        SegformerConfig,
+        SegformerForSemanticSegmentation,
     )
-    testmodel = SegformerForSemanticSegmentation(test_config)
-else:
-    testmodel = SegformerForSemanticSegmentation.from_pretrained(
-        load_from,
-        id2label=id2label,
-        label2id=label2id,
-        local_files_only=True,
+    import pathlib as pl
+    import datetime
+    import os
+    import accelerate
+    import shutil
+    import torch.nn.functional as F
+    os.environ["WANDB_PROJECT"] = "glacformer_training"
+    os.environ["NCCL_CUMEM_ENABLE"] = "0"
+    os.environ["NCCL_DEBUG"] = "INFO"
+    torch.backends.cuda.matmul.allow_tf32 = True
+    parent_dir = pathlib.Path(__file__).resolve().parent
+
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--continue_training",
+        type=bool,
+        help="continues training from the last checkpoint",
+        default=False,
     )
 
-transform = A.Compose(
-    [
-        A.ElasticTransform(p=0.5, alpha=1, sigma=25),
-        A.GridDistortion(p=0.5, distort_limit=.3),
-        A.RandomBrightnessContrast(p=0.8),
-        A.RandomToneCurve(p=0.8),
-    ],
-    additional_targets={"mask": "mask"},
-)
+    parser.add_argument(
+        "--load_from",
+        type=str,
+        help="load a new model or from a checkpoint or a path. if 'new' or a path, set continue_training to false",
+        default="new",
+    )
 
-# Define a function to apply transformations to a batch of training examples
-def train_transforms(example_batch):
-    imagesandmasks = [
-        transform(image=np.array(image.convert("RGB")), mask=np.array(mask))
-        for image, mask in zip(example_batch["image"], example_batch["label"])
-    ]
-    # applies the transform to the image and mask, but the data is stored as a list of dictionaries, so the next lines separate out the dicts into 2 different lists
-    images = [i["image"] for i in imagesandmasks]
-    masks = [i["mask"] for i in imagesandmasks]
-    inputs = test_image_processor(images, masks)
-    return inputs
+    parser.add_argument(
+        "--token",
+        type=str,
+        help="The throwaway auth token",
+        default="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",
+    )
+    parser.add_argument(
+        "--learning_rate",
+        type=float,
+        help="The initial learning rate for Adam",
+        default=18e-5,
+    )
+    parser.add_argument(
+        "--num_epochs",
+        type=int,
+        help="Total number of training epochs to perform",
+        default=1,
+    )
 
-# Define a function to apply transformations to a batch of validation examples
-def val_transforms(example_batch):
-    # Convert each image in the batch to RGB
-    images = [x.convert("RGB") for x in example_batch["image"]]
-    labels = [x for x in example_batch["label"]]
-    inputs = test_image_processor(images, labels)
-    return inputs
+    args = parser.parse_args()
+    token = args.token
+    learning_rate = args.learning_rate
+    num_epochs = args.num_epochs
+    load_from = args.load_from
 
-# this makes the transforms happen when a batch is loaded
-train_ds = train_ds.with_transform(train_transforms)
-test_ds = test_ds.with_transform(val_transforms)
+    if args.continue_training == "True":
+        load_from = "checkpoint"
+        print("Continuing training from checkpoint")
+    else:
+        print("Starting new training")
+        
+    hf_model_name = "glacierscopessegmentation/glacier_segmentation_transformer"
+    huggingface_hub.login(token=token, add_to_git_credential=True)
 
-# Load the "mean_iou" metric for evaluating semantic segmentation models
-metric = evaluate.load("mean_iou")
+    ds = load_dataset("glacierscopessegmentation/scopes", keep_in_memory=True, cache_dir=pl.Path(__file__).parent / "data")
+    train_ds = ds["train"]
+    test_ds = ds["test"]
+    ds.cleanup_cache_files()
+    del ds
 
-# Define a function to compute metrics for evaluation predictions
-# Here, the metric is mean intersection over union
-def compute_metrics(eval_pred):
-    # Ensure that gradient computation is turned off, as it is not needed for evaluation
-    with torch.no_grad():
-        # This computes the final logits tensor by interpolating the output logits to the size of the labels tensor from an input of size (batch_size, num_labels, height, width)
-        # This is input that has gone through the model's forward pass
-        logits, labels = eval_pred
-        logits_tensor = torch.from_numpy(logits).cpu()
+    id2label = {
+        "0": "sky",  # This is given by the rgb value of 00 00 00 for the mask
+        "1": "surface-to-bed",  # This is given by the rgb value of 01 01 01 for the mask
+        "2": "bed-to-bottom",  # This is given by the rgb value of 02 02 02 for the mask
+    }
 
-        logits_tensor = logits_tensor.argmax(dim=1)
-        logits_tensor = logits_tensor.unsqueeze(1).to(float).detach().cpu()
-        # this can lead to very high ram usage for the upscaling
-        logits_tensor = nn.functional.interpolate(
-            logits_tensor,
-            size=labels.shape[-2:],
-            mode="bilinear",
-            align_corners=False,
-        )
+    id2label = {int(k): v for k, v in id2label.items()}
+    label2id = {v: k for k, v in id2label.items()}
+    num_labels = len(id2label)
+    print(len(train_ds), len(test_ds))
+    train_ds.cleanup_cache_files()
+    test_ds.cleanup_cache_files()
+    test_image_processor = SegformerImageProcessor.from_pretrained("nvidia/MiT-b0")
 
-        logits_tensor = torch.squeeze(logits_tensor, dim=1)
-        pred_labels = logits_tensor.detach().cpu()
-        # Computes metrics
-        metrics = metric.compute(
-            predictions=pred_labels,
-            references=labels,
+    if load_from == "new" or load_from == "checkpoint":
+        test_config = SegformerConfig(
+            num_channels=3,
             num_labels=num_labels,
-            reduce_labels=False,
-            ignore_index=255,
+            label2id=label2id,
+            id2label=id2label,
+            depths=[3, 4, 6, 3],
+            hidden_sizes=[64, 128, 320, 512],
+            num_attention_heads=[1, 2, 5, 8],
+            decoder_hidden_size=128 * 6,
         )
-        for key, value in metrics.items():
-            if type(value) is np.ndarray:
-                metrics[key] = value.tolist()
-        # Return the computed metrics
-        return metrics
+        testmodel = SegformerForSemanticSegmentation(test_config)
+    else:
+        testmodel = SegformerForSemanticSegmentation.from_pretrained(
+            load_from,
+            id2label=id2label,
+            label2id=label2id,
+            local_files_only=True,
+        )
 
-# Define the training arguments
-if load_from == "checkpoint":
-    run_name = sorted(list((parent_dir.parent / "glacformer").glob("*")))[-1].name
-else: run_name = datetime.datetime.now().strftime("SherlockCluster--%Y-%m-%d--%H-%M-%S-%Z")
+    transform = A.Compose(
+        [
+            A.ElasticTransform(p=0.5, alpha=1, sigma=25),
+            A.GridDistortion(p=0.5, distort_limit=.3),
+            A.RandomBrightnessContrast(p=0.8),
+            A.RandomToneCurve(p=0.8),
+        ],
+        additional_targets={"mask": "mask"},
+    )
 
-training_args = TrainingArguments(
-    output_dir = "glacformer/" + run_name,
-    # overwrite_output_dir=True,
-    learning_rate=learning_rate,  # The initial learning rate for Adam
-    num_train_epochs=num_epochs,  # Total number of training epochs to perform
-    auto_find_batch_size=True,  # Whether to automatically find an appropriate batch size
-    save_total_limit=3,  # Limit the total amount of checkpoints and delete the older checkpoints
-    # eval_accumulation_steps=1,  # Number of steps to accumulate gradients before performing a backward/update pass
-    eval_strategy="epoch",  # The evaluation strategy to adopt during training
-    save_strategy="epoch",  # The checkpoint save strategy to adopt during training
-    save_steps=100,  # Number of update steps before two checkpoint saves
-    eval_steps=1,  # Number of update steps before two evaluations
-    logging_steps=.5,  # Number of update steps before logging learning rate and other metrics
-    remove_unused_columns=False,  # Whether to remove columns not used by the model when using a dataset
-    fp16=True,  # Whether to use 16-bit float precision instead of 32-bit for saving memory
-    tf32=True,  # Whether to use tf32 precision instead of 32-bit for saving memory
-    # gradient_accumulation_steps=4,  # Number of updates steps to accumulate before performing a backward/update pass for saving memory
-    hub_model_id=hf_model_name,  # The model ID on the Hugging Face model hub
-    report_to="wandb",
-    run_name=run_name,
-    per_device_train_batch_size=100,  
-    ignore_data_skip=True,
-    push_to_hub=True
-)
+    # Define a function to apply transformations to a batch of training examples
+    def train_transforms(example_batch):
+        imagesandmasks = [
+            transform(image=np.array(image.convert("RGB")), mask=np.array(mask))
+            for image, mask in zip(example_batch["image"], example_batch["label"])
+        ]
+        # applies the transform to the image and mask, but the data is stored as a list of dictionaries, so the next lines separate out the dicts into 2 different lists
+        images = [i["image"] for i in imagesandmasks]
+        masks = [i["mask"] for i in imagesandmasks]
+        inputs = test_image_processor(images, masks)
+        return inputs
 
-# Define the trainer
-trainer = Trainer(
-    model=testmodel,
-    args=training_args,
-    train_dataset=train_ds,
-    eval_dataset=test_ds,
-    compute_metrics=compute_metrics,
-)
+    # Define a function to apply transformations to a batch of validation examples
+    def val_transforms(example_batch):
+        # Convert each image in the batch to RGB
+        images = [x.convert("RGB") for x in example_batch["image"]]
+        labels = [x for x in example_batch["label"]]
+        inputs = test_image_processor(images, labels)
+        return inputs
 
-if load_from == "checkpoint": trainer.train(resume_from_checkpoint=True)
-else: trainer.train()
+    # this makes the transforms happen when a batch is loaded
+    train_ds = train_ds.with_transform(train_transforms)
+    test_ds = test_ds.with_transform(val_transforms)
 
+    # Load the "mean_iou" metric for evaluating semantic segmentation models
+    metric = evaluate.load("mean_iou",cache_dir="./eval_cache")
+
+    # Define a function to compute metrics for evaluation predictions
+    # Here, the metric is mean intersection over union
+    
+    @record
+    def compute_metrics(eval_pred):
+        with torch.no_grad():
+            logits, labels = eval_pred
+            labels = labels
+            print("\n\n\t \t \t \t \tPre Interpolation\n\n")
+            # Interpolate logits to match the size of labels
+            pred_labels = F.interpolate(torch.from_numpy(logits).cpu().argmax(dim=1).unsqueeze(1).float(), size=labels.shape[-2:], mode="bilinear", align_corners=False).squeeze(1).cpu()
+            print("\n\n\t \t \t \t \tPost Interpolation\n\n")
+            print(pred_labels.shape,labels.shape)
+            # Compute metrics
+            metrics = metric.compute(
+                predictions=pred_labels,
+                references=labels,
+                num_labels=num_labels,
+                reduce_labels=False,
+                ignore_index=255,
+            )
+            print("\n\n\t \t \t \t \tPost Computation\n\n")
+
+            # Convert numpy arrays to lists
+            metrics = {key: value.tolist() if isinstance(value, np.ndarray) else value for key, value in metrics.items()}
+            m2 = dict()
+            for key in metrics:
+                if isinstance(metrics[key],list):
+                    for c,v in enumerate(metrics[key]):
+                        m2[key + "_" + id2label[c]] = v
+                    else:
+                        m2[key] = v
+            print("\n\n\t \t \t \t \tFinished reformatting Metrics\n\n")
+            del metrics
+            return m2
+
+    # Define the training arguments
+    if load_from == "checkpoint":
+        run_name = sorted(list((parent_dir.parent / "glacformer").glob("*")))[-1].name
+    else: run_name = datetime.datetime.now().strftime("SherlockCluster--%Y-%m-%d--%H-%M-%S-%Z")
+
+    training_args = TrainingArguments(
+        output_dir = "glacformer/" + run_name,
+        # overwrite_output_dir=True,
+        learning_rate=learning_rate,  # The initial learning rate for Adam
+        num_train_epochs=num_epochs,  # Total number of training epochs to perform
+        auto_find_batch_size=True,  # Whether to automatically find an appropriate batch size
+        save_total_limit=3,  # Limit the total amount of checkpoints and delete the older checkpoints
+        # eval_accumulation_steps=1,  # Number of steps to accumulate gradients before performing a backward/update pass
+        eval_strategy="epoch",  # The evaluation strategy to adopt during training
+        save_strategy="epoch",  # The checkpoint save strategy to adopt during training
+        save_steps=100,  # Number of update steps before two checkpoint saves
+        eval_steps=1,  # Number of update steps before two evaluations
+        logging_steps=.5,  # Number of update steps before logging learning rate and other metrics
+        remove_unused_columns=False,  # Whether to remove columns not used by the model when using a dataset
+        fp16=True,  # Whether to use 16-bit float precision instead of 32-bit for saving memory
+        tf32=True,  # Whether to use tf32 precision instead of 32-bit for saving memory
+        # gradient_accumulation_steps=4,  # Number of updates steps to accumulate before performing a backward/update pass for saving memory
+        hub_model_id=hf_model_name,  # The model ID on the Hugging Face model hub
+        report_to="wandb",
+        run_name=run_name,
+        per_device_train_batch_size=116, 
+        per_device_eval_batch_size = 1, 
+        ignore_data_skip=True,
+        push_to_hub=True,
+        ddp_find_unused_parameters=False,
+        # deepspeed=./config.json
+    )
+
+    # Define the trainer
+    trainer = Trainer(
+        model=testmodel,
+        args=training_args,
+        train_dataset=train_ds,
+        eval_dataset=test_ds,
+        compute_metrics=compute_metrics,
+    )
+
+    if load_from == "checkpoint": trainer.train(resume_from_checkpoint=True)
+    else: trainer.train()
+
+if __name__ == "__main__":
+    main()
